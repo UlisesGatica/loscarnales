3ede6c048910bea606ec9dc7b3c3b6448985adc9
UlisesGatica <gaticaulises26@gmail.com>
2024-11-11 16:22:08 -0700

    fixing url

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/loscarnales">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
